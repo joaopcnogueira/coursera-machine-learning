460f45653c33e3beb4187a064c19c3073eee773a
joaopcnogueira <joaonogueira@fisica.ufc.br>
2018-10-31 19:09:37 -0300

    Week 7 completed

--- Coursera/Week-7/ex6/dataset3Params.m
+++ Coursera/Week-7/ex6/dataset3Params.m
@@ -23,7 +23,44 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+min_error = 15628; % just a big number
 
+for C_test = [0.01 0.03 0.1 0.3 1 3 10 30]
+  for sigma_test = [0.01 0.03 0.1 0.3 1 3 10 30]
+    model = svmTrain(X, y, C_test, @(x1,x2) gaussianKernel(x1, x2, sigma_test));
+    predictions = svmPredict(model, Xval);
+    predictions_errors = mean(double(predictions ~= yval));
+    
+    if predictions_errors < min_error
+      min_error = predictions_errors;
+      C = C_test;
+      sigma = sigma_test;
+    end
+end
+
+
+%%% Another implementation
+
+%results = eye(64,3);
+%pos = 0;
+%min_error = 15628; % just a big number
+%
+%for C_test = [0.01 0.03 0.1 0.3 1 3 10 30]
+%  for sigma_test = [0.01 0.03 0.1 0.3 1 3 10 30]
+%    model = svmTrain(X, y, C_test, @(x1,x2) gaussianKernel(x1, x2, sigma_test));
+%    predictions = svmPredict(model, Xval);
+%    predictions_errors = mean(double(predictions ~= yval));
+%    
+%    pos = pos + 1;
+%    results(pos,:) = [C_test, sigma_test, predictions_errors];
+%    
+%  end
+%end
+%
+%sorted_results = sortrows(results, 3); 
+%
+%C = sorted_results(1,1);
+%sigma = sorted_results(1,2);
 
 
 
